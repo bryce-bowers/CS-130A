13767b5bc198ecaa08c7d8ef7de443e82f4fed1c
bryce-bowers <bbowers@umail.ucsb.edu>
2015-10-16 15:10:22 -0700

    Everything works

--- wall.cpp
+++ wall.cpp
@@ -25,44 +25,43 @@ bool Wall::CreateWallFromString(string wall_posts_as_string)
   const string new_wall_post_delimiter=";nwp&"; //indicates start of new WallPost
   const string new_attribute_delimiter=";na&"; //indicates next attribute for WallPost
 
-      delimiter_position = wall_posts_as_string.find(wall_username_delimiter);
-      wall_post_strings[0] = wall_posts_as_string.substr(0, delimiter_position);      
-      Wall* newPost = new Wall(wall_post_strings[0]);
-      wall_posts_as_string.erase(0, delimiter_position + 4);
-      cout << wall_post_strings[0] << endl;
-      Wall newWall(wall_post_strings[0]);
-  //while(wall_posts_as_string.find(new_wall_post_delimiter) > wall_posts_as_string.find(new_attribute_delimiter))
-for(int i = 0; wall_posts_as_string.find(new_attribute_delimiter) < wall_posts_as_string.find(new_wall_post_delimiter) && i < wall_post_strings_size; i++)
-    { 
-      for(int j = 0; j < wall_post_strings_size; j++) //is there a way to find size of the array? this hurts me
-        {
-          wall_post_strings[j] = "";
-        }
+  delimiter_position = wall_posts_as_string.find(wall_username_delimiter);
+  wall_post_strings[0] = wall_posts_as_string.substr(0, delimiter_position);      
+  Wall* newPost = new Wall(wall_post_strings[0]);
+  wall_posts_as_string.erase(0, delimiter_position + 4);
+  Wall newWall(wall_post_strings[0]);
 
-      //for(int i = 0; wall_posts_as_string.find(new_attribute_delimiter) < wall_posts_as_string.find(new_wall_post_delimiter) && i < wall_post_strings_size; i++)
-        //{
-  
+  while (wall_posts_as_string.find(new_attribute_delimiter) < wall_posts_as_string.find(new_wall_post_delimiter))
+    {
+      for(int i = 0; i < wall_post_strings_size; i++) //is there a way to find size of the array? this hurts me
+        {
+          wall_post_strings[i] = "";
+        }
+      for (int i = 0; wall_posts_as_string.find(new_attribute_delimiter) < wall_posts_as_string.find(new_wall_post_delimiter); i++)
+        {
+	  delimiter_position = wall_posts_as_string.find(new_attribute_delimiter);
           wall_post_strings[i] = wall_posts_as_string.substr(0, delimiter_position);
           wall_posts_as_string.erase(0, delimiter_position+4);
-          cout << wall_post_strings[i] << " ";          
-        //}
-        wall_posts_as_string.erase(0, 5);
-      //create new WallPost
+        }
+      delimiter_position = wall_posts_as_string.find(new_wall_post_delimiter);
+      wall_posts_as_string.erase(0, 5); //deletes new wall post delimiter
+
       WallPost* newWallPost = new WallPost(wall_post_strings[0], wall_post_strings[1]);
-        for(int i = 0; i < wall_post_strings_size; i++)
-          {
-            
-            switch(i)
-              {
-            case 2:
+      for(int i = 0; i < wall_post_strings_size; i++)
+        {
+          switch(i)
+            {
+	    case 2:
               if (wall_post_strings[i] != "")
                 {
-                newWallPost->SetTimeCreated(wall_post_strings[i]);
+                  newWallPost->SetTimeCreated(wall_post_strings[i]);
                 }
-              }
-          }
-        newWall.AddPost(newWallPost);
-  }
+	      break;
+            }
+        }
+      newWall.AddPost(newWallPost);
+    }
+  return true;
 }
 
 

3da6550fcf3f849917cb5f5ba209cdae52913f4d
Anna Luo <AnnaLuo@Annas-iMac.local>
2015-11-01 12:42:23 -0800

    modified list.h

--- list.h
+++ list.h
@@ -1,13 +1,28 @@
 #ifndef LIST_H
 #define LIST_H
 #include "doubly_linked_list.h"
+using namespace std;
 
 template <class T>
 class List {
 private:
 	DoublyLinkedList<T>* linked_list;
-	int length;	
-public:
+	int length;
+
+public:	
+	class iterator
+        {
+            public:
+                iterator(T* ptr) : ptr_(ptr) { }
+                iterator operator++() { iterator i = *this; ptr_++; return i; }
+                iterator operator++(int junk) { ptr_++; return *this; }
+                T& operator*() { return *ptr_; }
+                T* operator->() { return ptr_; }
+                bool operator==(const iterator& rhs) { return ptr_ == rhs.ptr_; }
+                bool operator!=(const iterator& rhs) { return ptr_ != rhs.ptr_; }
+            private:
+                T* ptr_;
+        };
 	List();
 	~List();
 
